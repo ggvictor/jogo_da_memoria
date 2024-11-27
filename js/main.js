d570d0dca8b84422eed6aadda19015d9b0fb2c5e
ggvictor <victorrodriguessilva2@gmail.com>
2024-11-27 19:27:21 -0300

    timer

--- js/main.js
+++ js/main.js
@@ -1,14 +1,31 @@
 // elementos
 const number = document.querySelector("#number");
-const vader = document.querySelectorAll(".vader")
-const cards = document.querySelectorAll(".cards")
+const vader = document.querySelectorAll(".vader");
+const cards = document.querySelectorAll(".cards");
+const body = document.querySelector("#body")
 // funções
+
+function timeNumber(){
+    let numberInt = Number(number.textContent);
+    setTimeout(() => {
+        const interval = setInterval(() => {
+            if (numberInt >= 200) { // Condição de parada
+                clearInterval(interval); // Para o incremento
+                console.log("Finalizado:", numberInt);
+            } else {
+                numberInt += 1; // Incrementa o valor
+                number.innerHTML = numberInt // Mostra o valor atual
+            }
+        }, 1000); // Incrementa a cada 50ms
+    }, 1000); // Aguarda 1 segundo antes de começar
+}
 
 function flipCard(card) {
     card.classList.toggle("flipped");
     if (card.classList.contains("flipped")) {
         card.style.transform = "rotateY(180deg)";
-    } else {
+    } 
+    else {
         card.style.transform = "rotateY(0)";
     }
 
@@ -28,8 +45,8 @@ function checkCards(){
                     card.style.transform = "rotateY(0)";
                 });
             }, 1000); // Dá um tempo para mostrar as cartas antes de reverter
-        }
-    }
-}
+        };
+    };
+};
 // eventos
-addEventListener('click', () => gameDouble());
+window.addEventListener('load', ()=> timeNumber());
